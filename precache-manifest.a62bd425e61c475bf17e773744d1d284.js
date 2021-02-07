d0bf08ee71a3ded2b054cc0f409818701c465f75
adityanz <aditya@adityadixit.com>
2021-02-06 17:21:04 -0800

    Updates

---
+++ precache-manifest.a62bd425e61c475bf17e773744d1d284.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2099c697d7afb80c6b698d95783bc5",
+    "url": "/healthcare-dashboard/index.html"
+  },
+  {
+    "revision": "91835b99472084c4699e",
+    "url": "/healthcare-dashboard/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "c71b71a0c5d1aa36014a",
+    "url": "/healthcare-dashboard/static/css/main.9cf8a415.chunk.css"
+  },
+  {
+    "revision": "91835b99472084c4699e",
+    "url": "/healthcare-dashboard/static/js/2.eede569d.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/healthcare-dashboard/static/js/2.eede569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71b71a0c5d1aa36014a",
+    "url": "/healthcare-dashboard/static/js/main.149fbeaa.chunk.js"
+  },
+  {
+    "revision": "e1c450546bc10906a7e5",
+    "url": "/healthcare-dashboard/static/js/runtime-main.db3ad30e.js"
+  },
+  {
+    "revision": "dabb7594d4fd237fa5bc095585d77823",
+    "url": "/healthcare-dashboard/static/media/aditya.dabb7594.jpeg"
+  },
+  {
+    "revision": "8ae35631f922687123a0c391a17ca562",
+    "url": "/healthcare-dashboard/static/media/chia.8ae35631.jpeg"
+  },
+  {
+    "revision": "443c2d0e1f074fc645dcf4e133ef9569",
+    "url": "/healthcare-dashboard/static/media/jochem.443c2d0e.jpeg"
+  },
+  {
+    "revision": "357b37122d8f8e996469900748cbf2d0",
+    "url": "/healthcare-dashboard/static/media/stokala.357b3712.jpeg"
+  },
+  {
+    "revision": "bd49ff315039c914c92f0101c863f4bf",
+    "url": "/healthcare-dashboard/static/media/tim.bd49ff31.jpg"
+  }
+]);
